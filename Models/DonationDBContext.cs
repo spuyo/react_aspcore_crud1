03925a0544824825fbc3bcf1c902f0c0487fa01f
zane <zane.gray@humanmode.com>
2020-12-01 12:06:31 -0600

    index migration

--- Models/DonationDBContext.cs
+++ Models/DonationDBContext.cs
@@ -2,12 +2,19 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ReactAspCoreCrud1.Models
 {
-    public class DonationDBContext : DbContext
-    {
-        public DonationDBContext(DbContextOptions<DonationDBContext> options) : base(options)
-        {
-            
-        }
-        public DbSet<DCandidate> DCandidates { get; set; }
-    }
+	public class DonationDBContext : DbContext
+	{
+		public DonationDBContext(DbContextOptions<DonationDBContext> options) : base(options)
+		{
+		}
+		protected override void OnModelCreating(ModelBuilder builder){
+			builder.Entity<DCandidate>(
+					e => {
+					e.HasIndex(e => e.fullName);
+
+					}
+					);
+		}
+		public DbSet<DCandidate> DCandidates { get; set; }
+	}
 }
